c9c9ded15b4317b89c8c93462220fda5366ecb3a
aiden <259509332+onepersonzittys@users.noreply.github.com>
2026-04-07 22:45:04 -0300

    Create firstSQL1.sql for table1 operations
    Add SQL script to manage table1 with inserts, updates, and a query.

---
+++ firstSQL1.sql
@@ -0,0 +1,21 @@
+USE base1;
+
+TRUNCATE TABLE table1;
+
+-- insert the items into the table
+INSERT INTO table1 (name, color, variable) VALUES 
+('item1', 'black', 50.00),
+('item2', 'white', 150.00),
+('item4', 'black', 20.00);
+
+-- updates the table
+UPDATE table1 
+SET color = 'black', variable = 45.00 
+WHERE id = 2;
+
+-- filters in decresent mode
+SELECT id, name, variable
+FROM table1
+WHERE color = 'black' AND variable < 100
+ORDER BY variable DESC
+LIMIT 10;
