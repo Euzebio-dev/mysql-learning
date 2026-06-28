5611fbb12d8d0cc76aa47a7dfba288d444a46206
Gabriel Euzebio <igorvairo315@gmail.com>
2026-06-27 23:51:19 -0300

    Set default values for issued_date, activation_time, and created_at

--- First-SQL/time-stamp.sql
+++ First-SQL/time-stamp.sql
@@ -7,13 +7,13 @@
 CREATE TABLE credit_cards (
     card_id INT PRIMARY KEY,
     card_number VARCHAR(16),
-    issued_date DATE, --gets the current date, ex. 2026-11-02, and makes it the issued date
-    activation_time TIME, --gets the time, ex. 22:32:12pm and makes it the activation time
-    created_at TIMESTAMP --gets the date and hour and inserts it into the "created at" column
+    issued_date DATE DEFAULT CURRENT_DATE, --gets the current date, ex. 2026-11-02, and makes it the issued date
+    activation_time TIME DEFAULT CURRENT_TIME, --gets the time, ex. 22:32:12pm and makes it the activation time
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP --gets the date and hour and inserts it into the "created at" column
 );
 
 INSERT INTO credit_cards (card_id, card_number, issued_date, activation_time, created_at)
-VALUES (1, '4111111111111111', '2026-06-27', '22:15:00', '2026-06-27 22:15:00');
+VALUES (1, '4111111111111111');
 
 SELECT 
     card_number,
